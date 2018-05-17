2e1312a2c8f388cd8ff7ab5d1e8713023039eb93
Reena <reena.sanjay.sharma@gmail.com>
2018-05-17 22:08:56 +0100

    passing test for returning Plang if n is a factor of 5

--- src/raindrops.js
+++ src/raindrops.js
@@ -3,9 +3,11 @@ class Raindrops {
   numberToSound(n) {
     if (n % 3 === 0) {
       return "Pling";
+    } else if (n % 5 === 0) {
+      return "Plang";
     }
   }
-  
+
 }
 
 module.exports = Raindrops;
--- test/raindrops.test.js
+++ test/raindrops.test.js
@@ -4,9 +4,17 @@ const should = chai.should();
 const Raindrops = require('../src/raindrops.js');
 
 describe('Raindrops',() => {
-  
+  let raindrops;
+
+  beforeEach(() => {
+    raindrops = new Raindrops();
+  });
+
   it("returns pling if n is a factor of 3",() => {
-    let raindrops = new Raindrops();
     raindrops.numberToSound(6).should.be.equal('Pling');
+  })
+
+  it("returns plang if n is a factor of 5",() => {
+    raindrops.numberToSound(10).should.be.equal('Plang');
   })
 });
